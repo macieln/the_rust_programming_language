f38ffffa8ece6845def3b94c55030c9dbc4021fa
Nahum Maciel <nahumamaciel@gmail.com>
2025-07-16 12:23:35 -0700

    Continued to work through chapter 3 exercises.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,4 @@
-use std::u32;
+use std::io;
 
 fn main() {
 //    let mut x = 5;
@@ -25,10 +25,90 @@ fn main() {
 //    let mut spaces = "    ";
 //    spaces = spaces.len();
 
-    let guess: u32 = "42"
+//    let guess: u32 = "42"
+//        .parse()
+//        .expect("Not a number!");
+
+//    let x = 2.0;
+//
+//    let y: f32 = 3.0;
+
+//    let sum = 5 + 10;
+//
+//    let difference = 95.5 - 4.3;
+//
+//    let product = 4 * 30;
+//
+//    let quotient = 56.7 / 32.2;
+//    let truncated = -5 / 3;
+//
+//    let remainder = 43 % 5;
+
+//    let t = true;
+//    let f: bool = false;
+
+//    let c = 'z';
+//    let z: char = 'Z';
+//    let heart_eyed_cat = '😻';
+
+//    let tup: (i32, f64, u8) = (500, 6.4, 1);
+//
+//    let (x, y, z) = tup;
+
+//    let x: (i32, f64, u8) = (500, 6.4, 1);
+//
+//    let five_hundred = x.0;
+//
+//    let six_point_four = x.1;
+//
+//    let one = x.2;
+//
+//    println!("The value of (five_hundred) is: {five_hundred}");
+//    println!("The value of (six_point_four) is: {six_point_four}");
+//    println!("The value of (one) is: {one}");
+
+//    let a = [1, 2, 3, 4, 5];
+//    let months = [
+//        "January",
+//        "February",
+//        "March",
+//        "April",
+//        "May",
+//        "June",
+//        "July",
+//        "August",
+//        "September",
+//        "October",
+//        "November",
+//        "December"
+//    ];
+//
+//    let a = [3; 5];
+
+//    let a = [1, 2, 3, 4, 5];
+//
+//    let first = a[0];
+//    let second = a[1];
+//
+//    println!("first: {}", first);
+//    println!("second: {}", second);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
         .parse()
-        .expect("Not a number!");
+        .expect("Index entered was not a number.");
 
+    let element = a[index];
 
-
+    println!("The value of the element at index {index} is: {element}.");
 }
--- variables/target/debug/deps/variables-91410a1c4c9e04ac.d
+++ variables/target/debug/deps/variables-91410a1c4c9e04ac.d
@@ -1,5 +1,5 @@
-C:\Users\Nahum\Documents\the_rust_programming_language\variables\target\debug\deps\variables-91410a1c4c9e04ac.d: src\main.rs
+C:\the_rust_programming_language\variables\target\debug\deps\variables-91410a1c4c9e04ac.d: src\main.rs
 
-C:\Users\Nahum\Documents\the_rust_programming_language\variables\target\debug\deps\libvariables-91410a1c4c9e04ac.rmeta: src\main.rs
+C:\the_rust_programming_language\variables\target\debug\deps\libvariables-91410a1c4c9e04ac.rmeta: src\main.rs
 
 src\main.rs:
